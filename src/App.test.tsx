bac64c32d619093801fe84df6147a5df7c3224ff
masayukinii1011 <masayukinii1011@gmail.com>
2021-03-22 19:21:50 +0900

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+//import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+
+    screen.debug();
+
+    //文字列(完全一致)が存在しているか。
+    //正規表現(部分一致)も使用可能
+    //getBy => 存在しない要素のアサーションを行おうとすると、エラーをスローする。
+    //toBeInTheDocument => アサーション関数
+    expect(screen.getByText("Search:")).toBeInTheDocument();
+
+    //queryBy => 存在しない要素のアサーションを行う場合
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    //findBy => まだ存在しないものの最終的に存在する要素のアサーションを行う場合
+    //非同期要素
+
+    //DOMの属性
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  test("fireEvent", () => {
+    render(<App />);
+
+    screen.debug();
+
+    //存在しないことを確認して
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    //イベントの発火をシミュレート
+    //fireEvent.change(要素, イベント)
+    //userEventの方が推奨される
+    fireEvent.change(screen.getByRole("textbox"), {
+      target: { value: "JavaScript" },
+    });
+    //存在を確認
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+
+    screen.debug();
+  });
+
+  test("userEvent", async () => {
+    render(<App />);
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    //userEvent.type(要素, イベント)
+    //fireEventよりも実際のブラウザに近い振る舞い
+    //changeだけでなく、keyDown、keyPress、keyUpイベントの発火
+    await userEvent.type(screen.getByRole("textbox"), "JavaScript");
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [search, setSearch] = React.useState("");
+
+  const handleChange = (event: any) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+
+      <p>Searches for {search ? search : "..."}</p>
     </div>
   );
-}
+};
+
+type SearchProps = {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  children: string;
+};
+
+const Search: React.FC<SearchProps> = ({ value, onChange, children }) => {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" type="text" value={value} onChange={onChange} />
+    </div>
+  );
+};
 
 export default App;
